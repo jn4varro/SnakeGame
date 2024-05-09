5f0cbe8fff0d56894041b04620c221b1384abf89
jn4varro <jnavarro.artist@gmail.com>
2024-05-09 16:46:05 +0200

    Primeros pasos

---
+++ SnakeGame.py
@@ -0,0 +1,33 @@
+import turtle
+import time
+import random
+
+#Crear ventana principal
+ventana = turtle.Screen()
+ventana.title("SnakeGame")
+turtle.colormode(255)
+ventana.bgcolor(155, 188, 15)
+ventana.setup(width=600, height=600)
+ventana.tracer(0)
+
+#Crear la cabeza de la serpiente
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color(48 ,98, 48)
+head.goto(0,0)
+head.penup()
+head.direction = "stop"
+
+#Crear objetivo de la serpiente
+goal = turtle.Turtle()
+goal.speed(0)
+goal.penup()
+goal.shape("circle")
+goal.color(48 ,98, 48)
+goal.goto(0, 100)
+
+
+
+while True:
+    ventana.update()
